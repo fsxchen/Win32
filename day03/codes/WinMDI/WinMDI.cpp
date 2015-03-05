f05e5520421b231ee51e31f649d3e73d118f9e27
fsxchen <fsxchen@gmail.com>
2015-03-05 15:45:45 +0800

    fsxchen

--- day03/codes/WinMDI/WinMDI.cpp
+++ day03/codes/WinMDI/WinMDI.cpp
@@ -3,12 +3,27 @@
 
 #include "stdafx.h"
 
+HWND g_hMDIClient = NULL;
+
+LRESULT CALLBACK MainProc(HWND hWnd,
+						  UINT nMsg,
+						  WPARAM wParam,
+						  LPARAM lParam) 
+{
+	return DefFrameProc(hWnd, g_hMDIClient, nMsg,
+		wParam, lParam);
+}
+
+
+
+
 int APIENTRY WinMain(HINSTANCE hInstance,
                      HINSTANCE hPrevInstance,
                      LPSTR     lpCmdLine,
                      int       nCmdShow)
 {
  	// TODO: Place code here.
+
 
 	return 0;
 }
